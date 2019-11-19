3c2eaa7479daff6c654079f6f866de11d8f4e5c9
pavels81 <schechkov.pavel@yandex.ru>
2019-11-19 10:37:16 +0300

    Add SplashScreen, created POJO classes

---
+++ app/src/main/java/com/shechkov/dntest/ui/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.shechkov.dntest.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.shechkov.dntest.MainActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
